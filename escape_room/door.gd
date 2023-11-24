b9dd550e07df23b88ef239b690f1019e1150e16d
Bernardo Leandro <80755587+BernardoLeandro1@users.noreply.github.com>
2023-11-24 18:04:46 +0000

    rework on door animation

--- escape_room/door.gd
+++ escape_room/door.gd
@@ -3,20 +3,19 @@ extends StaticBody3D
 
 var open = false
 var in_animation = false
-@onready var anim = get_node("dooropen")
+@onready var player := $"../../player"
 @onready var d := $dooropen
+@onready var doorway := $"."
 
 func _process(delta):
 	interact()
 
 
 func interact():
-	if Input.is_action_just_pressed("ui_interact") and !open:
-#		tween.interpolate_property(self, "rotation_degrees", Vector3(0, rotation_degrees.y, 0), Vector3(0, rotation_degrees.y+90, 0), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-#		in_animation = true
+	if Input.is_action_just_pressed("ui_interact") and !open and 33.5 < player.get_position().distance_to(doorway.get_position())  and player.get_position().distance_to(doorway.get_position()) < 37.0:
 		d.play("Open")
 		open = true
-	elif Input.is_action_just_pressed("ui_interact") and open:
+	elif Input.is_action_just_pressed("ui_interact") and open and 33.5 < player.get_position().distance_to(doorway.get_position())  and player.get_position().distance_to(doorway.get_position()) < 37.0:
 		d.play_backwards("Open")
 		open = false
 
