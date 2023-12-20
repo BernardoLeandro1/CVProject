7281f17cccffd5c7d3f84f2b7d17ae443260c3bd
Bernardo Leandro <80755587+BernardoLeandro1@users.noreply.github.com>
2023-12-20 17:45:26 +0000

    living room done

---
+++ escape_room/ClockFace.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0673024c330661b41145d2f321214272693fadc1d54fc1dcd1b1ce2c4e8bd7f
+size 171
--- escape_room/door.gd
+++ escape_room/door.gd
@@ -1,21 +1,3 @@
-extends StaticBody3D
-
-
-var open = false
-var in_animation = false
-@onready var player := $"../../player"
-@onready var d := $dooropen
-@onready var doorway := $"."
-
-func interact():
-	if !open:
-		d.play("Open")
-		open = true
-	elif open:
-		d.play_backwards("Open")
-		open = false
-
-
-
-
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:7bb589213f4a12ca0001c5ec39ed53aa06667f0b352eebe9adf6b6f77a7e7465
+size 290
