b08ca7bbc3a29753bedfe8d445560b636e6a31e2
Bernardo Leandro <80755587+BernardoLeandro1@users.noreply.github.com>
2023-12-20 16:14:30 +0000

    player crouching

--- escape_room/scripts/player.gd
+++ escape_room/scripts/player.gd
@@ -4,17 +4,21 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const mouse_sens = 0.4
-
+const CROUCHING_SPEED = 20
+const CROUCHED_SPEED = 2.0
+const HEIGHT = 1.8
+const CROUCH_HEIGHT = 0.8
 
 @onready var head = $head
 @onready var camera := $head/Camera3D
 @onready var interaction := $head/Camera3D/interaction
 @onready var hand := $head/Camera3D/hand
+@onready var pcap := $CollisionShape3D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var picked_object
 var pull_power = 10
-
+var crouched = false
 
 
 func pick_object():
@@ -33,6 +37,16 @@ func drop_object():
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 func _process(delta):
+	if Input.is_action_just_pressed("ui_crouch") and !crouched:
+		crouched = true
+		pcap.shape.height -= CROUCHING_SPEED * delta
+		head.position.y = pcap.shape.height
+	elif Input.is_action_just_pressed("ui_crouch") and crouched:
+		crouched = false
+		pcap.shape.height += CROUCHING_SPEED * delta
+		head.position.y = pcap.shape.height
+	
+	pcap.shape.height = clamp(pcap.shape.height, CROUCH_HEIGHT, HEIGHT)
 	var collider = interaction.get_collider()
 	if collider != null:
 		if Input.is_action_just_pressed("ui_interact") and collider.get_script() != null:
@@ -70,9 +84,12 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	if direction and !crouched:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+	elif direction and crouched:
+		velocity.x = direction.x * CROUCHED_SPEED
+		velocity.z = direction.z * CROUCHED_SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
