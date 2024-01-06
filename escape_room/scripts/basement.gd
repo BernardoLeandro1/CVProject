5073740d5a9104ea87e27c4b36a8fef56326aff5
Bernardo Leandro <80755587+BernardoLeandro1@users.noreply.github.com>
2024-01-06 11:23:51 +0000

    added win sound

--- escape_room/scripts/basement.gd
+++ escape_room/scripts/basement.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d882329bbfec048fc3c959d7cc2be0ced5cdd079417fd36c490b356c9fbc04ea
-size 1551
+oid sha256:7cfe4af949d3ac982a5b24a1b171da6682dfe427e2cd8ddce163538be0f00046
+size 1926
