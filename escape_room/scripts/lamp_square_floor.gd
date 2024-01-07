0396d1fe7cba86c1155ee8f7e57e733c0a33f0f6
Bernardo Leandro <80755587+BernardoLeandro1@users.noreply.github.com>
2024-01-07 16:48:15 +0000

    error fixing

--- escape_room/scripts/lamp_square_floor.gd
+++ escape_room/scripts/lamp_square_floor.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f473982a36fbbe48d08ea1be8df093add159f36c57b19e1d73fea7d2ea5690a
-size 406
+oid sha256:e108963bebb2504f47b036fff07d5e10eeaeaa6ca9532ab7418256f41cb5ba68
+size 532
